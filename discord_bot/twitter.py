a9d7702d5798c090175a5b2004cb19aa47b6964e
YoungAndY2m <2604424450@qq.com>
2022-12-02 15:44:14 -0500

    twitter.py minor change

--- discord_bot/twitter.py
+++ discord_bot/twitter.py
@@ -1,16 +1,17 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
-query = "(from:elonmusk) until:2022-12-02" \
+query = "(from:MasterDaye) until:2022-12-02" \
         "since:2022-11-01"
 tweets = []
-limits = 100
+limits = 10
 
 for tweet in sntwitter.TwitterSearchScraper(query).get_items():
     if len(tweets) == limits:
         break
     else:
-        tweets.append([tweet.date, tweet.user.username, tweet.content])
+        tweets.append([tweet.date, tweet.user.username, tweet.media])
 
 df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
 print(df)
+df.to_csv(f'here.csv', index=False, encoding='utf-8')
